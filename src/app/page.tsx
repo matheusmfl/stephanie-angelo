6a88880713384d695ff7afc6142546131ac9b7ca
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2024-03-02 21:34:50 -0300

    feat: add images, add frame-motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+import { AboutStephanie } from '@/components/AboutStephanie'
+import { ClinicLocation } from '@/components/ClinicLocation'
 import { ClinicSpecializations } from '@/components/ClinicSpecializations'
 import { Header } from '@/components/Header'
 import { Hero } from '@/components/Hero'
+import { ProcedureCount } from '@/components/ProcedureCount'
+import { TeamIntroduction } from '@/components/TeamIntroduction'
 
 export default function Home() {
   return (
@@ -8,6 +12,10 @@ export default function Home() {
       <Header />
       <Hero />
       <ClinicSpecializations />
+      <AboutStephanie />
+      <ClinicLocation />
+      <ProcedureCount />
+      <TeamIntroduction />
     </div>
   )
 }
