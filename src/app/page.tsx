c2abdac0e2e41cb3da52bc2ed443cc68c58df184
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2025-03-12 17:10:10 -0300

    fix lint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { AboutCourses } from '@/components/AboutCourses'
 import { AboutStephanie } from '@/components/AboutStephanie'
 import { ClinicLocation } from '@/components/ClinicLocation'
-import { ClinicSpecializations } from '@/components/ClinicSpecializations'
 import { CoursesTimeline } from '@/components/CoursesTimeline'
 import { Depositions } from '@/components/Depositions'
 import { FormContact } from '@/components/FormContact'
@@ -12,7 +11,6 @@ import { ProcedureCount } from '@/components/ProcedureCount'
 import { TeamIntroduction } from '@/components/TeamIntroduction'
 
 export default function Home() {
-
   return (
     <div className="overflow-x-hidden">
       <Hero />
